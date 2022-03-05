20cbae66c8931f238e116e008e2801b55d1028b3
Pinaki Roy <thepinakiroy@gmail.com>
2022-03-05 11:39:07 +0530

    Updates

---
+++ precache-manifest.8d27489d40ea315f5bc45c7a9df534e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f82c353f37cb428f6a73fff000e2fd",
+    "url": "/cut/index.html"
+  },
+  {
+    "revision": "db2d6f9df6a25815355c",
+    "url": "/cut/static/css/main.d1eb618d.chunk.css"
+  },
+  {
+    "revision": "326a7f762b466019aff4",
+    "url": "/cut/static/js/2.6c98f0b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cut/static/js/2.6c98f0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2d6f9df6a25815355c",
+    "url": "/cut/static/js/main.1d7bb29c.chunk.js"
+  },
+  {
+    "revision": "894d853d340ba07f7091",
+    "url": "/cut/static/js/runtime-main.fef9af55.js"
+  }
+]);
